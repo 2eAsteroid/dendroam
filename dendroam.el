f5c59a7226b1a48d27862292695958db5a46bc40
2e Asteroid <two-e-asteroid@outlook.com>
2024-06-30 17:50:20 -0400

    Fix dendroam.el

--- dendroam.el
+++ dendroam.el
@@ -24,65 +24,61 @@
 (provide 'dendroam)
 
 ;;Node custom getters
-(cl-defmethod org-roam-node-current-file (node)
-  "Gets node file-name-base by file name"
-  (file-name-base (org-roam-node-file node)))
-
 (cl-defmethod org-roam-node-hierarchy-title (node)
-  "Gets node title excluding the hierarchy and capitalize it"
-  (capitalize
-   (car
-    (last
-     (split-string
-      (org-roam-node-title node)
-      "\\.")))))
+  "Gets node title excluding the hierarchy and capitalize it."
+  (capitalize (car (last (split-string (org-roam-node-title node) (rx "."))))))
+
+(cl-defmethod org-roam-node-spacing (node)
+  (string-pad " " (- 25 (length (funcall 'dendroam-format-hierarchy (org-roam-node-file node))))))
 
 (defun dendroam-format-hierarchy (file)
-  "Formats node's path, to get the hierarchy whithout the title
+  "Formats node's path, to get the hierarchy without the title
 where title will be the last child of the hierarchy:
 from the filename this.is.a.hierarchy.note-title.org
 returns this.is.a.hierarchy"
   (let* ((base-name (file-name-base file))
+         (hierarchy-p (> (cl-count ?\. base-name) 0))
          (hierarchy-no-title (file-name-base base-name)))
-    hierarchy-no-title))
+    (if hierarchy-p
+        (concat hierarchy-no-title ".")
+      "")))
 
 (cl-defmethod org-roam-node-hierarchy (node)
-  "Gets node hierarchy by file name"
+  "Gets hierarchy of NODE by file name."
   (funcall 'dendroam-format-hierarchy (org-roam-node-file node)))
 
 (cl-defmethod org-roam-node-current-file (node)
-  (file-name-base (buffer-file-name)))
+  "Gets the name of the current NODE."
+  (file-name-base (org-roam-node-file node)))
 
 ;; Refactor functions
 
 (defun dendroam-fetch-same-hierarchy-files (hierarchy)
   "Gets all the nodes that share the same HIERARCHY totally or parcially"
-  (let ((files
-         (mapcar #'car (org-roam-db-query [:select [file]
-                                           :from nodes
-                                           :where (like file $r1)]
-                                          (concat "%" hierarchy "%")))))
+  (let ((files (mapcar #'car (org-roam-db-query [:select [file]
+                                                         :from nodes
+                                                         :where (like file $r1)]
+                                                (concat "%" hierarchy "%")))))
     files))
 
 (defun dendroam-refactor-hierarchy (&optional current)
   "Prompts the user to change the hierarchy of the current file
 node and updates its hierarchy and the hierarchy of all the nodes
-that have it if CURRENT is t the list of updated files is just
-the current file"
+that have it. if CURRENT is non-nil, the list of updated files is just
+the current file."
   (interactive)
-  (let*
-      ((initial-file
-        (file-name-nondirectory (buffer-file-name)))
-       (initial-slug
-        (file-name-base initial-file))
-       (new-slug (file-name-base
-                  (read-string "Refactor: " initial-slug)))
-       (initial-slug-no-title
-        (file-name-base initial-slug))
-       (files-to-upd (if current
-                         `(,initial-file)
-                       (dendroam-fetch-same-hierarchy-files
-                        initial-slug-no-title))))
+  (let* ((initial-file
+          (file-name-nondirectory (buffer-file-name)))
+         (initial-slug
+          (file-name-base initial-file))
+         (new-slug (file-name-base
+                    (read-string (format "Refactor (from %s to ): " initial-slug) initial-slug)))
+         (initial-slug-no-title
+          (file-name-base initial-slug))
+         (files-to-upd (if current
+                           `(,initial-file)
+                         (dendroam-fetch-same-hierarchy-files
+                          initial-slug-no-title))))
 
     (dolist (file files-to-upd)
       (let ((new-file
@@ -93,40 +89,30 @@ the current file"
               (kill-current-buffer)
               (find-file new-file)))))))
 
-(defun dendroam-refactor-file ()
-  (interactive)
-  (let* ((initial-file (buffer-file-name))
-         (initial-slug (file-name-base initial-file))
-         (new-slug (read-string "Refactor: " initial-slug))
-         (new-file (concat
-                    (expand-file-name new-slug org-roam-directory)
-                    ".org")))
-    (rename-file initial-file new-file)
-    (kill-current-buffer)
-    (find-file new-file)))
-
-;; Useful notes functions
-(defun dendroam-insert-time-note(&optional goto)
-  "Creates a time note in the current level of the hierarchy.
-Time notes have the format: current.Y.m.d.MS3N
-The file is created using a template from `dendroam-capture-templates'"
-  (interactive "P")
-  (org-roam-capture- :goto (when goto '(4))
-                     :node (org-roam-node-create)
-                     :templates dendroam-capture-templates
-                     :keys "t"
-                     :props (list :default-time (current-time))))
-
-(defun dendroam-insert-scratch-note(&optional goto)
-  "Creates a time note in the current level of the hierarchy.
-Time notes have the format: current.Y.m.d.MS3N
-The file is created using a template from `dendroam-capture-templates'"
-  (interactive "P")
-  (org-roam-capture- :goto (when goto '(4))
-                     :node (org-roam-node-create)
-                     :templates dendroam-capture-templates
-                     :keys "s"
-                     :props (list :default-time (current-time))))
+(defvar-local org-roam-slug-trim-chars
+    '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
+      768 ; U+0300 COMBINING GRAVE ACCENT
+      769 ; U+0301 COMBINING ACUTE ACCENT
+      770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
+      771 ; U+0303 COMBINING TILDE
+      772 ; U+0304 COMBINING MACRON
+      774 ; U+0306 COMBINING BREVE
+      775 ; U+0307 COMBINING DOT ABOVE
+      776 ; U+0308 COMBINING DIAERESIS
+      777 ; U+0309 COMBINING HOOK ABOVE
+      778 ; U+030A COMBINING RING ABOVE
+      780 ; U+030C COMBINING CARON
+      795 ; U+031B COMBINING HORN
+      803 ; U+0323 COMBINING DOT BELOW
+      804 ; U+0324 COMBINING DIAERESIS BELOW
+      805 ; U+0325 COMBINING RING BELOW
+      807 ; U+0327 COMBINING CEDILLA
+      813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+      814 ; U+032E COMBINING BREVE BELOW
+      816 ; U+0330 COMBINING TILDE BELOW
+      817 ; U+0331 COMBINING MACRON BELOW
+      )
+  "Characters to remove in the slug of an org roam node.")
 
 ;; Org roam overrides to allow these features
 (eval-after-load "org-roam"
@@ -134,20 +120,22 @@ The file is created using a template from `dendroam-capture-templates'"
      "Override. Generates a dendron-like slug from *title*
 this expects an input like: lang.elisp.what is nil
 and will create a file wih the name: lang.elisp.what-is-nil"
-     (let ((title (org-roam-node-title node)))
-       (cl-flet* ((nonspacing-mark-p (char)
-                                     (memq char org-roam-slug-trim-chars))
-                  (strip-nonspacing-marks (s)
-                                          (ucs-normalize-NFC-string
-                                           (apply #'string (seq-remove #'nonspacing-mark-p
-                                                                       (ucs-normalize-NFD-string s)))))
-                  (cl-replace (title pair)
-                              (replace-regexp-in-string (car pair) (cdr pair) title)))
-         (let* ((pairs `(("[^[:alnum:][:digit:]_.]" . "-")  ;; convert anything not alphanumeric except "."
-                         (" " . "-")    ;; remove whitespaces
-                         ("__*" . "-")  ;; remove sequential underscores
-                         ("^_" . "")  ;; remove starting underscore
-                         ("_$" . "")))  ;; remove ending underscore
+     (let ((title (org-roam-node-title node))) ; Gets the title of the node
+       ;; Defining simple functions
+       (cl-flet* ((nonspacing-mark-p (char) ; Checks if character is an accent / combining thing
+                    (memq char org-roam-slug-trim-chars))
+                  (strip-nonspacing-marks (s) ; Decompose the string and remove all accents
+                    (ucs-normalize-NFC-string
+                     (apply #'string (seq-remove #'nonspacing-mark-p
+                                                 (ucs-normalize-NFD-string s)))))
+                  (cl-replace (title pair) ; Replace parts in the title with another thing
+                    (replace-regexp-in-string (car pair) (cdr pair) title)))
+         ;; Defining replacement pairs
+         (let* ((pairs `((,(rx (+ (not (any "." alnum digit)))) . "-")  ;; convert anything not alphanumeric except "."
+                         (,(rx (+ " ")) . "-")    ;; remove whitespaces
+                         (,(rx "_" (* "_")) . "-")  ;; remove sequential underscores
+                         (,(rx line-start "_") . "")  ;; remove starting underscore
+                         (,(rx "_" line-end) . "")))  ;; remove ending underscore
                 (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs)))
            (downcase slug))))))
 ;;; dendroam.el ends here
